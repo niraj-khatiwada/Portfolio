8e17d32bed1332a74a89bcbdcac784380c5ce09a
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-06-09 00:13:50 +0545

    Redwsigned

--- js/text/javascript.js
+++ js/text/javascript.js
@@ -1,32 +1,25 @@
 var layer1 = document.getElementById('layer1')
-scroll = window.pageYOffset;
-document.addEventListener('scroll',
- function(e)
- {
-     var offset = window.pageYOffset;
-     scroll = offset;
-     layer1.style.width = (100 + scroll/5) +  '%';
- });
+scroll = window.pageYOffset
+document.addEventListener('scroll', function (e) {
+  var offset = window.pageYOffset
+  scroll = offset
+  layer1.style.width = 100 + scroll / 5 + '%'
+})
 
 var layer2 = document.getElementById('layer2')
-scroll = window.pageYOffset;
-document.addEventListener('scroll',
- function(e)
- {
-     var offset = window.pageYOffset;
-     scroll = offset;
-     layer2.style.width = (100 + scroll/5) +'%';
-     layer2.style.left =scroll/50 +  '%';
- });
+scroll = window.pageYOffset
+document.addEventListener('scroll', function (e) {
+  var offset = window.pageYOffset
+  scroll = offset
+  layer2.style.width = 100 + scroll / 5 + '%'
+  layer2.style.left = scroll / 50 + '%'
+})
 
 var text = document.getElementById('text')
-scroll = window.pageYOffset;
-document.addEventListener('scroll',
- function(e)
- {
-     var offset = window.pageYOffset;
-     scroll = offset;
-     layer2.style.width = (100 + scroll/5) +'%';
-     text.style.top = - scroll/11 +  '%';
- });
-
+scroll = window.pageYOffset
+document.addEventListener('scroll', function (e) {
+  var offset = window.pageYOffset
+  scroll = offset
+  layer2.style.width = 100 + scroll / 5 + '%'
+  text.style.top = -scroll / 5 + '%'
+})
